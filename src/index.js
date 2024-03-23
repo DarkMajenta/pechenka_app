2dfaa6133e357c5ba78f38533bf9576ced567a14
drk_mjnt <borovskij.a@edu.narfu.ru>
2024-03-24 01:58:44 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './components/App';  // Check the correct path to App component
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
